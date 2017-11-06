12d33850bf556a1ef78b2b774b89461d6a275dc4
PierrePlessy <pierre.plessy@outlook.fr>
2017-11-06 16:19:20 +0100

    bad

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,21 +6,6 @@ class TagsController < ApplicationController
 
   def show
     @pictures = Picture.find_by_tag(params[:id])
-    # @pictures = []
-    # @tags = []
-
-    # # Search pictures by tag
-    # tpictures.each do |tpic|
-    #   pictu = Picture.findById(tpic.picture.id)
-    #   @pictures += pictu
-    # end
-    #
-    # #Search all the tags for all the picture
-    # @pictures.each do |picture|
-    #   tag = TagPicture.findByPictures(picture.id)
-    #   @tags += tag
-    # end
-
   end
 
 end
