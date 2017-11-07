854ed820130dc77d78be1bcc1378881f5ed13740
PierrePlessy <pierre.plessy@outlook.fr>
2017-11-07 17:09:23 +0100

    Rebuild upload

---
+++ test/models/picture_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class PictureTest < ActiveSupport::TestCase
+
+  test "the truth" do
+    assert true
+
+
+    # listings = Listing.search(keywords: 'iphone')
+    #
+    # assert_equal 1, listings.length
+    # assert_equal 'iphone title', listings.first.title
+  end
+
+end
